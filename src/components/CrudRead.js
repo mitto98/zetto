210807b1f19bc6c7c0c98639c753363524c340e3
Mattia Dapino <mattia.dapino@hey.com>
2021-10-08 16:41:08 +0200

    feat: Added support to display preloaded entity

--- src/components/CrudRead.js
+++ src/components/CrudRead.js
@@ -1,22 +1,23 @@
 import { mergeComponentFields } from '../lib/fields';
 import titledMixin, { titledMixinProps } from '../mixins/titledMixin';
 import listenOnRoot from '../mixins/listenOnRoot';
+import translatorMixin from '../mixins/translatorMixin';
 import { EVENT_NAME_REFRESH_DATA } from '../constants/events';
 import { Detail } from '../models/bootstrap4';
 
 export default {
   name: 'CrudRead',
-  mixins: [titledMixin, listenOnRoot],
+  mixins: [titledMixin, listenOnRoot, translatorMixin],
   props: {
     id: { type: String },
     action: { type: [Object, String], required: true },
-    fields: { type: Array, default: null },
-    entity: { type: [String, Number], required: true },
+    fields: { type: Object, default: () => ({}) },
+    entity: { type: [Object, String, Number], required: true },
     ...titledMixinProps,
   },
   data: () => ({
-    loading: true,
-    item: null,
+    loading: false,
+    loadedItem: null,
   }),
   mounted() {
     this.listenOnRoot(EVENT_NAME_REFRESH_DATA, this.refreshDataHandler);
@@ -25,11 +26,11 @@ export default {
   computed: {
     tableFields() {
       if (!this.action) return [];
-
-      return mergeComponentFields(
-        this.action.getSummaryAttributes(),
-        this.fields
-      );
+      return mergeComponentFields(this.action.properties, this.fields);
+    },
+    item() {
+      if (this.entity._isPortofinoEntity) return this.entity;
+      return this.loadedItem;
     },
   },
   methods: {
@@ -37,12 +38,14 @@ export default {
       if (this.id && this.id === id) await this.fetchData();
     },
     async fetchData() {
+      if (this.entity._isPortofinoEntity) return;
       this.loading = true;
-      this.item = await this.action.get(this.entity);
+      this.loadedItem = await this.action.get(this.entity);
       this.loading = false;
     },
   },
   render(h) {
+    if (this.loading) return h('p', this.$trans('zetto.search.loading'));
     return h('div', [
       h('h1', this.elementTitle),
       h(Detail, {
