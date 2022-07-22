27a7dec225973fe44cdd793d824388d96425688c
Mattia Dapino <mattia.dapino@hey.com>
2022-07-22 13:35:02 +0200

    feat: added es and fr locales

---
+++ src/locales/es.ts
@@ -0,0 +1,21 @@
+export default {
+  zetto: {
+    search: {
+      no_results: 'Ningun resultado',
+      loading: 'Cargando...',
+      filter: 'Filtros',
+      filter_clean: 'Quita filtros',
+      fields: {
+        mode: {
+          contains: 'Contenido',
+          equals: 'Igual a',
+          starts: 'Empieza con',
+          ends: 'Termina con',
+        },
+        range_from: 'Desde {label}',
+        range_to: 'Hasta {label}',
+        loading_more: 'Cargando más opciones...',
+      },
+    },
+  },
+};
---
+++ src/locales/fr.ts
@@ -0,0 +1,21 @@
+export default {
+  zetto: {
+    search: {
+      no_results: 'Pas de résultat',
+      loading: 'Chargement...',
+      filter: 'Filtre',
+      filter_clean: 'Nettoyer les filtres',
+      fields: {
+        mode: {
+          contains: 'Contient',
+          equals: "C'est la même chose que",
+          starts: 'Commencer pour',
+          ends: 'Se termine pour',
+        },
+        range_from: '{label} de',
+        range_to: '{label} à',
+        loading_more: "Charger d'autres options...",
+      },
+    },
+  },
+};
